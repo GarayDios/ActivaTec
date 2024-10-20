f4fc60bb437d36b20361f1f35af0a7464c6306cb
Danie <Danie@Daniel-Garay>
2024-10-20 01:25:16 -0600

    wachen culos la clase estudiante

---
+++ src/Estudiante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Danie
+ */
+public class Estudiante {
+    private String Carrera;
+    private String nocontrol;
+    
+    private String semestre;
+    
+}
