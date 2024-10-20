d6dd1f6471e87b7ef471b896700d0055334af159
52311 <52311@OswaldoDiaz>
2024-10-20 01:06:41 -0600

    Persona

---
+++ src/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 52311
+ */
+public class Persona {
+    private String nombre;
+    private byte edad;
+    private int AMaterno;
+    private int APaterno;
+    private String fechsNacimiento;
+    private String ocupacion;
+    
+}
