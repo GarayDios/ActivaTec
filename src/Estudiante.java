eec310a781cf35082cce2894be27cd7aad34896c
Danie <Danie@Daniel-Garay>
2024-10-20 01:50:28 -0600

    Actualizacion de la clase Estudiante

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -10,7 +10,39 @@
 public class Estudiante {
     private String Carrera;
     private String nocontrol;
-    
     private String semestre;
-    
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String Carrera, String nocontrol, String semestre) {
+        this.Carrera = Carrera;
+        this.nocontrol = nocontrol;
+        this.semestre = semestre;
+    }
+
+    public String getCarrera() {
+        return Carrera;
+    }
+
+    public void setCarrera(String Carrera) {
+        this.Carrera = Carrera;
+    }
+
+    public String getNocontrol() {
+        return nocontrol;
+    }
+
+    public void setNocontrol(String nocontrol) {
+        this.nocontrol = nocontrol;
+    }
+
+    public String getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+        
 }
